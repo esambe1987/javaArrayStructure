44432f4932e858e43d1bc048b1ea7383d5cc867c
esambe1987 <64872528+esambe1987@users.noreply.github.com>
2021-02-05 11:51:02 -0600

    initial commit

---
+++ src/javaArrayStructure/javaArrayStructure.java
@@ -0,0 +1,51 @@
+package javaArrayStructure;
+import java.util.Arrays;
+
+
+import java.lang.Math;
+
+         public class javaArrayStructure{
+         public static void main(String [] args) {
+         
+         int my_array[] = {1, 5, 6, 5, 4,};
+		 int sum = 0;
+
+		for (int i : my_array)
+	
+			sum += i;
+		
+		
+			System.out.println("The sum is " + sum);{
+				
+		}
+			
+	}
+      
+			   
+			   
+			 
+         public static main{    
+    
+	public static double[]: (javaArrayStructure) double[]  toPower(int size, int power){
+        double[] result = new double[size];
+        for(int i=0; i<size; i++){
+            result[i] = Math.pow( Double.valueOf(i), Double.valueOf(power));
+        }
+        return result;
+    }
+      
+
+    public static void main1(String) [],args){
+    	
+    	
+        
+        int size = 4; 
+        int power = 2;
+        double[] result = toPower(size, power);
+        
+        System.out.print("toPower result is: "+(result));
+        
+    }
+    
+
+  
